1340d3b8aec7c7755667acff4881f192f3f2a2c0
shubh07sx <shubh.saxena07@gmail.com>
2018-02-18 21:31:04 +0530

    profile

--- server.js
+++ server.js
@@ -13,15 +13,15 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+app.get('/ui/photo.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'photo.jpg'));
 });
 
 
 // Do not change port, otherwise your app won't run on IMAD servers
 // Use 8080 only for local development if you already have apache running on 80
 
-var port = 80;
+var port = 9123;
 app.listen(port, function () {
   console.log(`IMAD course app listening on port ${port}!`);
 });
