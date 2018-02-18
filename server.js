1257f7cf1c7bbd98573b17eb6a0d55be2b22889f
shubh07sx <shubh.saxena07@gmail.com>
2018-02-18 22:06:55 +0530

    photochanged

--- server.js
+++ server.js
@@ -13,8 +13,8 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/home/shubh/Desktop/webapp/imad-app/ui/photo.jpg', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'photo.jpg'));
+app.get('/home/shubh/Desktop/webapp/imad-app/ui/jj.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'jj.jpg'));
 });
 
 
